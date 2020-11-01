85dfa34753dba6c9e9849217b50ef7dab83fa3ba
Vitalik <wital2010@gmail.com>
2020-11-01 18:57:46 +0300

    Start Verstka

---
+++ src/components/header-music/header-music.js
@@ -0,0 +1,8 @@
+import logo from '../../logo.svg';
+import cx from 'classnames';
+import styles from './header-music.module.css';
+function HeaderMusic() {
+  return <div className={styles.headerMusic}>ыыыы</div>;
+}
+
+export default HeaderMusic;
