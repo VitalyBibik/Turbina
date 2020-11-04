530a462d31db121249013b72ebf0cb159e891d6e
Vitalik <wital2010@gmail.com>
2020-11-05 00:40:33 +0300

    render main page is DOne

--- src/components/section-block/section-block.js
+++ src/components/section-block/section-block.js
@@ -4,35 +4,28 @@ import SectionText from '../section-text/section-text';
 import SectionTezis from '../section-tezis/section-tezis';
 
 function SectionBlock({ sectionBlock }) {
-  function renderTezis(sectionBlock) {
-    return (
+  return (
+    <>
+      <>
+        {sectionBlock.mainText.map((item) => (
+          <div className={styles['section-block']} key={item.id}>
+            <SectionTitle title={item.title} />
+            {item.text.length > 1 ? (
+              item.text.map((item, index) => (
+                <SectionText text={item} key={index} />
+              ))
+            ) : (
+              <SectionText text={item.text[0]} />
+            )}
+          </div>
+        ))}
+      </>
       <div className={styles['section-block']}>
+        <SectionTitle title={sectionBlock.tezisText[0].title} />
         <SectionTezis text={sectionBlock.tezisText} />
       </div>
-    );
-  }
-  function renderMainPage(sectionBlock) {
-    return sectionBlock.mainText.map((item) => {
-      return (
-        <div className={styles['section-block']} key={item.id}>
-          <SectionTitle title={item.title} />
-          {item.text.length > 1 ? (
-            item.text.map((item, index) => (
-              <SectionText text={item} key={index} />
-            ))
-          ) : (
-            <SectionText text={item.text[0]} />
-          )}
-        </div>
-      );
-    });
-  }
-  function renderAll(sectionBlock) {
-    return renderMainPage(sectionBlock);
-    renderTezis(sectionBlock);
-  }
-
-  return renderAll(sectionBlock);
+    </>
+  );
 }
 
 export default SectionBlock;
