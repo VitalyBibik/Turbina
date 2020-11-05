b1ff83d495ad415ed0a76932d516fd4c88838db5
Vitaly Bibik <wital2010@gmail.com>
2020-11-05 12:10:15 +0300

    ADD static formik

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -1,7 +1,39 @@
 import styles from './form.module.css';
+import { Field, Formik } from 'formik';
 
 function Form() {
-  return <div>ss</div>;
+  return (
+    <>
+      <Formik
+        initialValues={{
+          firstName: '',
+          lastName: '',
+          email: '',
+        }}
+        onSubmit={async (values) => {
+          await new Promise((r) => setTimeout(r, 500));
+          alert(JSON.stringify(values, null, 2));
+        }}
+      >
+        <Form>
+          <label htmlFor="firstName">First Name</label>
+          <Field id="firstName" name="firstName" placeholder="Jane" />
+
+          <label htmlFor="lastName">Last Name</label>
+          <Field id="lastName" name="lastName" placeholder="Doe" />
+
+          <label htmlFor="email">Email</label>
+          <Field
+            id="email"
+            name="email"
+            placeholder="jane@acme.com"
+            type="email"
+          />
+          <button type="submit">Отправить форму</button>
+        </Form>
+      </Formik>
+    </>
+  );
 }
 
 export default Form;
