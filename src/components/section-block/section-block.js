83e3543e00f0d7a4466249eaf6ff016f6bcf1025
Vitaly Bibik <wital2010@gmail.com>
2020-11-05 09:44:29 +0300

    remove div

--- src/components/section-block/section-block.js
+++ src/components/section-block/section-block.js
@@ -6,20 +6,18 @@ import SectionTezis from '../section-tezis/section-tezis';
 function SectionBlock({ sectionBlock }) {
   return (
     <>
-      <>
-        {sectionBlock.mainText.map((item) => (
-          <div className={styles['section-block']} key={item.id}>
-            <SectionTitle title={item.title} />
-            {item.text.length > 1 ? (
-              item.text.map((item, index) => (
-                <SectionText text={item} key={index} />
-              ))
-            ) : (
-              <SectionText text={item.text[0]} />
-            )}
-          </div>
-        ))}
-      </>
+      {sectionBlock.mainText.map((item) => (
+        <div className={styles['section-block']} key={item.id}>
+          <SectionTitle title={item.title} />
+          {item.text.length > 1 ? (
+            item.text.map((item, index) => (
+              <SectionText text={item} key={index} />
+            ))
+          ) : (
+            <SectionText text={item.text[0]} />
+          )}
+        </div>
+      ))}
       <div className={styles['section-block']}>
         <SectionTitle title={sectionBlock.tezisText[0].title} />
         <SectionTezis text={sectionBlock.tezisText} />
