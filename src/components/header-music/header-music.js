ad0cd2182017424a5a7dfcfba4e01d22d835cbef
Vitalik <wital2010@gmail.com>
2020-11-07 02:30:51 +0300

    Adaptive

--- src/components/header-music/header-music.js
+++ src/components/header-music/header-music.js
@@ -3,9 +3,12 @@ import Banner from '../banner/index';
 import cn from 'classnames';
 import Turbina from '../turbina/index';
 import SoundPlayer from '../audio-player/index';
+import React, { useState } from 'react';
 import { audioTracks } from '../../fixtures';
 
 function HeaderMusic() {
+  const [count, setCount] = useState(false);
+
   return (
     <div className={styles['container']}>
       <div
@@ -15,55 +18,58 @@ function HeaderMusic() {
         )}
       >
         <Banner />
-        <button
-          className={cn(
-            styles['header-music__button'],
-            styles['header-block_mobile']
-          )}
-        >
-          Стриминги
-        </button>
-        <ul className={cn(styles['header-block-ul'])}>
-          <li className={styles['header-block']}>
-            <a
-              href="/"
-              className={cn(styles['header-block'], styles['header-link'])}
-            >
+        <div>
+          <button
+            className={cn(
+              styles['header-music__button'],
+              styles['header-block_mobile']
+            )}
+            onclick={() => setCount(!count)}
+          >
+            Стриминги
+          </button>
+          <ul className={cn(styles['header-block-ul'])}>
+            <li className={styles['header-block']}>
+              <a
+                href="/"
+                className={cn(styles['header-block'], styles['header-link'])}
+              >
+                {' '}
+                Яндекс.Музыка &#8599;
+              </a>
+            </li>
+            <li className={styles['header-block']}>
               {' '}
-              Яндекс.Музыка &#8599;
-            </a>
-          </li>
-          <li className={styles['header-block']}>
-            {' '}
-            <a
-              href="/"
-              className={cn(styles['header-block'], styles['header-link'])}
-            >
+              <a
+                href="/"
+                className={cn(styles['header-block'], styles['header-link'])}
+              >
+                {' '}
+                Spotify &#8599;
+              </a>
+            </li>
+            <li className={styles['header-block']}>
               {' '}
-              Spotify &#8599;
-            </a>
-          </li>
-          <li className={styles['header-block']}>
-            {' '}
-            <a
-              href="/"
-              className={cn(styles['header-block'], styles['header-link'])}
-            >
+              <a
+                href="/"
+                className={cn(styles['header-block'], styles['header-link'])}
+              >
+                {' '}
+                Apple Music &#8599;
+              </a>
+            </li>
+            <li className={styles['header-block']}>
               {' '}
-              Apple Music &#8599;
-            </a>
-          </li>
-          <li className={styles['header-block']}>
-            {' '}
-            <a
-              href="/"
-              className={cn(styles['header-block'], styles['header-link'])}
-            >
-              {' '}
-              VK Music &#8599;
-            </a>
-          </li>
-        </ul>
+              <a
+                href="/"
+                className={cn(styles['header-block'], styles['header-link'])}
+              >
+                {' '}
+                VK Music &#8599;
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
       <Turbina />
       <SoundPlayer />
