2fb7cc2006b9f298730c1233c5e1a3dada58549e
Vitalik <wital2010@gmail.com>
2020-11-07 01:28:35 +0300

    change name

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <HeaderMusic />
-      <div className="test">
+      <div className="page">
         <Root />
       </div>
       <Footer />
