e7fb67fde7202c11a30b83ccf83da19e745eab43
Vitalik <wital2010@gmail.com>
2020-11-07 03:02:09 +0300

    Add mobile menu

--- src/components/header-music/header-music.js
+++ src/components/header-music/header-music.js
@@ -7,7 +7,7 @@ import React, { useState } from 'react';
 import { audioTracks } from '../../fixtures';
 
 function HeaderMusic() {
-  const [count, setCount] = useState(false);
+  const [open, setOpen] = useState(false);
 
   return (
     <div className={styles['container']}>
@@ -18,53 +18,51 @@ function HeaderMusic() {
         )}
       >
         <Banner />
-        <div>
+        <div className={styles['menu']}>
           <button
             className={cn(
               styles['header-music__button'],
               styles['header-block_mobile']
             )}
-            onclick={() => setCount(!count)}
+            onClick={() => setOpen(!open)}
           >
             Стриминги
           </button>
-          <ul className={cn(styles['header-block-ul'])}>
+          <ul
+            className={cn(
+              styles['header-block-ul'],
+              open ? styles['open'] : null
+            )}
+          >
             <li className={styles['header-block']}>
               <a
                 href="/"
                 className={cn(styles['header-block'], styles['header-link'])}
               >
-                {' '}
                 Яндекс.Музыка &#8599;
               </a>
             </li>
             <li className={styles['header-block']}>
-              {' '}
               <a
                 href="/"
                 className={cn(styles['header-block'], styles['header-link'])}
               >
-                {' '}
                 Spotify &#8599;
               </a>
             </li>
             <li className={styles['header-block']}>
-              {' '}
               <a
                 href="/"
                 className={cn(styles['header-block'], styles['header-link'])}
               >
-                {' '}
                 Apple Music &#8599;
               </a>
             </li>
             <li className={styles['header-block']}>
-              {' '}
               <a
                 href="/"
                 className={cn(styles['header-block'], styles['header-link'])}
               >
-                {' '}
                 VK Music &#8599;
               </a>
             </li>
