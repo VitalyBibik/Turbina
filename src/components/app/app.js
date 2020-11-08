b78898294aa2e2b83ea2a64299ca20a882b5c6d8
Vitalik <wital2010@gmail.com>
2020-11-09 00:04:28 +0300

    Change APp on app.css

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -6,7 +6,7 @@ import Footer from '../footer';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <HeaderMusic />
       <div className="page">
         <Root />
