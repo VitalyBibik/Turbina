9e4a6ac292703bf27749d2a73ab5c240aadb8038
Vitalik <wital2010@gmail.com>
2020-11-28 00:27:20 +0300

    Take animations

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -8,9 +8,24 @@ import { tracks } from '../../tracks';
 import ParticlesBg from 'particles-bg';
 
 function App() {
+  const animationArray = [
+    'color',
+    'lines',
+    'thick',
+    'circle',
+    'cobweb',
+    'square',
+    'tadpole',
+    'fountain',
+  ];
+  const min = 0;
+  const max = animationArray.length - 1;
+  let nextAnimation =
+    animationArray[Math.floor(min + Math.random() * (max + 1 - min))];
+
   return (
     <div className={styles['app']}>
-      <ParticlesBg type="random" bg={true} />
+      <ParticlesBg type={nextAnimation} bg={true} />
       <HeaderMusic config={config} tracks={tracks} />
       <div className={styles['page']}>
         <div className={styles['page__container']}>
